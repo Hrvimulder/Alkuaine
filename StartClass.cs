c6d060cff05f6867696c689e96d6cca36790fe66
Nikotype <n.suuronen@gmail.com>
2025-01-14 12:32:14 +0200

    Alkurivit

---
+++ StartClass.cs
@@ -0,0 +1,23 @@
+public class StartClass
+{
+    public static void Start()
+    {
+        while (true)
+        {
+            Console.WriteLine("Haluatko pelata (p) tai tarkastella tuloksia (t)?");
+            string input = Console.ReadLine().ToLower();
+            if (input == "p")
+            {
+                Console.WriteLine("Pelaa");
+            }
+            else if (input == "t")
+            {
+                Console.WriteLine("Tarkastele");
+            }
+            else
+            {
+                Console.WriteLine("Virheellinen syöte. Yritä uudelleen.");
+            }
+        }
+    }
+}
---
+++ program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Alkuaine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StartClass.Start();
+        }
+    }
+}
